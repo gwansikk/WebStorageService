7df95af43de7c84a83b4622a60761d206e202cff
gwansikk <seorit98@gmail.com>
2023-02-18 00:16:31 +0900

    A: Prefix, Client 연결

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setGlobalPrefix('api');
+
+  await app.listen(13621);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
